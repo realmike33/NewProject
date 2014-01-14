0b1a851e94d4f6b259d9cefbde9f92081d369c40
maseh87 <maseh87@gmail.com>
2014-01-13 22:19:50 -0800

    New Project

---
+++ app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+
+angular.module('newCalendarApp', [
+  'ngRoute',
+  'ui.date',
+  'ngAnimate'
+
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/calendar', {
+        templateUrl: 'views/calendar.html',
+        controller: 'CalendarCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/calendar.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+angular.module('newCalendarApp')
+  .controller('CalendarCtrl', function($scope) {
+    $scope.template = 'calendar',
+    $scope.myDate = '',
+    $scope.todos = [],
+    $scope.addToDo = function(todo) {
+      $scope.todos.push({todo:todo, date: $scope.myDate});
+      $scope.todo = ''; //makes the date and todo box return back to empty after adding
+      $scope.myDate = '';
+    };
+  });
